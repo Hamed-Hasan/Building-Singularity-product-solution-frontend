3c4813a2d8928edf0bbe58d59e8afacfea56fa90
Hamed Hasan <hamedhasan422421@gmail.com>
2023-12-26 18:08:16 +0300

    🚀 Added validation schema for login inputs. 📊 Implemented Yup for email and password validation in login form. 🌟 Created validation schema for signup inputs. 🏆 Integrated Yup for name, email, and password validation in signup form. 🔄 Refactored code structure in validation schemas.

---
+++ src/schema/authSchema.jsx
@@ -0,0 +1,12 @@
+import * as Yup from 'yup';
+
+export const loginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email address').required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
+
+export const signupSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
+  });
