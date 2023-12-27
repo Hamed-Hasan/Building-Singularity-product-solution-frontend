dc432b1bdd8d03c79c49bb429276f88a596ef25e
Hamed Hasan <hamedhasan422421@gmail.com>
2023-12-27 14:01:37 +0300

    🚀 Added HomeHero component for a captivating homepage. 🔍 Integrated SmallSection to showcase New Releases. 👩‍🎤 Included Artistes component for exploring diverse artists. �� Implemented TopCharts for tracking trending songs. 🌐 Enhanced responsiveness with Hide component in HomePage.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,34 @@
+import HomeHero from "../components/HomeHero";
+import SmallSection from "../components/SmallSection";
+import TopCharts from "../components/TopCharts";
+import Categories from "../components/Categories";
+import Search from "../components/Search";
+import { Grid, GridItem, Hide } from "@chakra-ui/react";
+import Artistes from "../components/Artistes";
+
+const HomePage = () => {
+	return (
+		<Grid
+			templateColumns={{ base: "1fr", lg: "repeat(8, 1fr)" }}
+			minH="100vh"
+			pl={{ base: 2, md: 14, lg: 12, xl: 0 }}
+			pb={24}
+			pt={{ base: 14, md: 4 }}>
+			<GridItem colSpan={5} p={4}>
+				{/* <Search /> */}
+				<HomeHero />
+				<SmallSection title="New Releases" endpoint="/songs/releases" />
+				<Artistes />
+				<SmallSection title="Popular Around You" endpoint="/songs/top" />
+			</GridItem>
+			<GridItem colSpan={3} p={4}>
+				<TopCharts />
+				<Hide below="md">
+					<Categories />
+				</Hide>
+			</GridItem>
+		</Grid>
+	);
+};
+
+export default HomePage;
